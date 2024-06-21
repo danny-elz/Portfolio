e1c67e8c2e4c17b61d11bae3603abe9c45a8d41c
danny <elzeindanny5@gmail.com>
2024-06-21 13:53:15 -0400

    Added JSON data support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-  import { Component, ChangeDetectorRef } from '@angular/core';
-  import { Personal, Project } from './classSetups';
-
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { Personal, Project } from './classSetups';
+import contentData from '../assets/data/content.json';   
+import projectData from '../assets/data/projects.json'; 
   @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -10,58 +11,15 @@
   export class AppComponent {
     title = 'portfolio';
     darkMode: boolean = false;
-  
+    contentData: Personal = contentData.ContentData;
+    projectData: Project[] = projectData. ProjectData;
+
     constructor(private cd: ChangeDetectorRef) {}
-  
+    
     toggleDarkMode(): void {
       this.darkMode = !this.darkMode;
       this.cd.detectChanges(); 
     }
-
-    mydata: Personal = {
-      first: "Danny", 
-      last: "Elzein",
-      role: "Software Engineer", 
-      email: "elzeind@sheridancollege.ca", 
-      linkedin: "https://www.linkedin.com/in/danny-elzein", 
-      phone: "6475682063",
-      about: "I am a solution oriented Software Developer, lifelong learner, and fitness enthusiast with a background in software development and network engineering. I enjoy development because of the satisfaction I get by overcoming challenges. I am motivated by the opportunity that software provides to positively impact the life of an individual, and the world as a whole.",
-      technology: "Angular, HTML, CSS, TS"
-    }
-
-  projects: Project[] = [
-      {
-          id: 1,
-          name: 'MAGIC GAMMA TELESCOPE DATA ANALYSIS WITH TPOT',
-          image: 'assets/images/dataset-card.jpg',
-          subtitle: 'This project aims to automate the process of applying machine learning to the MAGIC Gamma Telescope dataset using TPOT (Tree-based Pipeline Optimization Tool). TPOT is an AutoML tool that simplifies the selection and application of machine learning models.'
-      },
-      {
-          id: 2,
-          name: 'MEDIA STORE',
-          image: 'assets/images/media.jpeg',
-          subtitle: 'This Project was built utilizing the Spring framework and offers an intuitive solution for all your media needs.'
-      },
-      {
-          id: 3,
-          name: 'OCR RECEIPT AND INVOICE PARSING',
-          image: 'assets/images/receipt.jpeg',
-          subtitle: 'This Python project demonstrates how to parse receipts using Optical Character Recognition (OCR) to extract financial data automatically. Utilizing an OCR API, the script processes receipt images and converts them into structured data, including store names, addresses, itemized purchases, and totals.'
-      },
-      {
-          id: 4,
-          name: 'SUDOKU SOLVER',
-          image: 'assets/images/sudoku.png',
-          subtitle: 'This project utilizes the Backtracking Algorithm to solve Sudoku puzzles. The Backtracking Algorithm is a systematic method for exploring possible solutions to Sudoku grids while ensuring adherence to the Sudoku rules. It efficiently fills each cell, backtracking when necessary, until a solution is found or its determined that no solution exists.'
-      },
-      {
-          id: 5,
-          name: 'PAC-MAN',
-          image: 'assets/images/pacman.webp',
-          subtitle: 'This project was built off the pygame library, recreating the classic arcade experience.'
-      }
-  ];
-  
 
   }
 
---
+++ src/app/interfaceCP.ts
@@ -0,0 +1,17 @@
+export interface Projects {
+    id: number,
+    name: string,
+    image: string,
+    subtitle: string
+}
+
+export interface Personal {
+    first: string,
+    last: string,
+    role: string,
+    email: string,
+    linkedin: string,
+    phone: string,
+    about: string,
+    technology: string
+}
