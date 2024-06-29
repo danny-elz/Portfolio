d5223ad056dd4af2736fd25fda5ecc751e5929a7
danny <elzeindanny5@gmail.com>
2024-06-29 00:06:19 -0400

    added comments

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,19 +10,20 @@ export class ProjectsComponent {
   @Input() projects: Project[] = [];
   @Input() darkMode: boolean = false;
   
-  filterQuery: string = '';
+  query: string = '';
   filteredData: Project[] = [];
 
   ngOnInit() {
-    this.filterSearch();
+    this.filterSearch(); //My webpage was not loading my projects on initial launch so added this function to initialize data
+                         //Im just confused how I was able to get this to work without importing OnInit?
   }
 
   filterSearch() {
-    if(!this.filterQuery) {
-      this.filteredData = this.projects;
+    if(!this.query) { 
+      this.filteredData = this.projects;  //if query is empty assign all the projects to filteredData
     } else {
       this.filteredData = this.projects.filter(project=>
-        project.name.toLowerCase().includes(this.filterQuery.toLowerCase())
+        project.name.toLowerCase().includes(this.query.toLowerCase()) //if query is not empty then display the project whose name matches the query
       );
     }
   }
